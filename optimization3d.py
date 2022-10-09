ad9cdc7029d58b53ff97d783b4e6ebd5e1ed1f9e
TateStaples <56731352+TateStaples@users.noreply.github.com>
2022-10-09 15:30:31 -0400

    functioning optimization and display

--- optimization3d.py
+++ optimization3d.py
@@ -128,33 +128,30 @@ class Filter:
         self.H, self.Hs = self._prep_filter(rmin)  # fixme: add better names
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
-        return np.array(self.H*(x.flatten()/self.Hs.flatten()).T).reshape(x.shape)
+        return np.array(self.H*(x.flatten('F')/self.Hs.flatten('F')).T).reshape(x.shape, order="F")
 
     def _prep_filter(self, filter_radius: float) -> typing.Tuple[np.ndarray, np.ndarray]:
-        x_nodes, y_nodes, z_nodes = self.size
+        y_nodes, x_nodes, z_nodes = self.size
         total_nodes = x_nodes * y_nodes * z_nodes
         ceil_r = math.ceil(filter_radius)
         # PREPARE FILTER - something to do with ill-posedness
-        rough_len = total_nodes * (2 * ceil_r) ** 3
-        iH = np.ones(rough_len)  # total_nodes * (2 * (ceil_r - 1) + 1) ** 2)
-        jH = np.ones(iH.shape)
-        sH = np.zeros(iH.shape)  # weight factors
-        k = 0
+        iH = list()
+        jH = list()
+        sH = list()  # weight factors
         # iterates through nodes
         for k1 in range(0, z_nodes):
             for i1 in range(0, x_nodes):
                 for j1 in range(0, y_nodes):
                     e1 = k1 * x_nodes * y_nodes + i1 * y_nodes + j1  # 1d index
                     # iterates through neighbors
-                    for k2 in range(max(k1 - ceil_r, 0), min(k1 + (ceil_r), z_nodes)):
-                        for i2 in range(max(i1 - ceil_r, 0), min(i1 + (ceil_r), x_nodes)):
-                            for j2 in range(max(j1 - ceil_r, 0), min(j1 + (ceil_r), y_nodes)):
+                    for k2 in range(max(k1 - (ceil_r-1), 0), min(k1 + (ceil_r), z_nodes)):
+                        for i2 in range(max(i1 - (ceil_r-1), 0), min(i1 + (ceil_r), x_nodes)):
+                            for j2 in range(max(j1 - (ceil_r-1), 0), min(j1 + (ceil_r), y_nodes)):
                                 e2 = k2 * x_nodes * y_nodes + i2 * y_nodes + j2
-                                iH[k] = e1
-                                jH[k] = e2
-                                sH[k] = max(0, filter_radius - np.sqrt((i1 - i2) ** 2 + (j1 - j2) ** 2 + (
-                                            k1 - k2) ** 2))  # neighbor - distance = weight factor
-                                k = k + 1
+                                iH.append(e1)
+                                jH.append(e2)
+                                sH.append(max(0, filter_radius - np.sqrt((i1 - i2) ** 2 + (j1 - j2) ** 2 + (k1 - k2) ** 2)))  # neighbor - distance = weight factor
+        sH = np.array(sH); iH = np.array(iH); jH = np.array(jH);
         H = coo_matrix((sH, (iH, jH)), shape=(total_nodes, total_nodes)).tocsc()  # each row is coordinate, each item in row is the corresponding weight
         Hs = H.sum(1)  # the total weights of a node
         return H, Hs
@@ -183,7 +180,7 @@ class FEA:
 
         # setup node connections
         node_grd = np.array(range(0, (y_nodes + 1) * (x_nodes + 1))).reshape((y_nodes + 1, x_nodes + 1), order='F')
-        node_ids = node_grd[:-1, :-1].reshape((y_nodes * x_nodes, 1))
+        node_ids = node_grd[:-1, :-1].reshape((y_nodes * x_nodes, 1), order="F")
         nodeidz = np.array(range(0, (z_nodes - 1) * (y_nodes + 1) * (x_nodes + 1) + 1, (y_nodes + 1) * (x_nodes + 1)))
         node_ids = np.tile(node_ids, (1, len(nodeidz))) + nodeidz
         element_dof_vec = 3 * (node_ids + 1).T.flatten()  # the first node of each cube
@@ -196,7 +193,7 @@ class FEA:
         self.jK = np.kron(self.element_dof_mat, np.ones((1, 24))).flatten()
 
     def displace(self, density: np.ndarray):
-        elementwise_stiffness = ((self.material.element_stiffness.flatten()[np.newaxis]).T*(self.material.min_stiff+(density.flatten())**self.material.penal*(self.material.max_stiff-self.material.min_stiff))).flatten(order='F')  # figure 15
+        elementwise_stiffness = ((self.material.element_stiffness.flatten()[np.newaxis]).T*(self.material.min_stiff+density.flatten('F')**self.material.penal*(self.material.max_stiff-self.material.min_stiff))).flatten(order='F')  # figure 15 fixme
         global_stiffness = coo_matrix((elementwise_stiffness, (self.iK, self.jK)), shape=(self.num_dofs, self.num_dofs)).tocsc()
         global_stiffness = (global_stiffness + global_stiffness.T) / 2.0
         # Remove constrained dofs from matrix
@@ -220,14 +217,13 @@ class FEA:
 
 
 class Optimizer:
-    # todo: look into other ways to optimize (strength, young modulus)
     def __init__(self, shape: typing.Tuple[int, int, int]):
         self.shape = shape
         self.total_nodes = 1
         for dim in shape: self.total_nodes *= dim
-        self.dv = np.ones(shape)
-        self.dc = Filter.smoothen(np.ones(shape))  # in the paper this goes in sensistivity analysis, but it looks constant
-        self.strain = np.ones(shape)  # i think this is strain energy
+        self.dc = np.ones(shape)
+        self.dv = np.ones(shape)  # in the paper this goes in sensistivity analysis, but it looks constant
+        self.strain = np.ones(shape)  # i think this is strain energy todo: confirm this is strain)
         self.score = 0
         self.change = 0
 
@@ -243,10 +239,11 @@ class Optimizer:
         total_nodes = self.total_nodes
         # OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS - figure 17-28 (minimum compliance)
         # 3d
-        self.strain[:] = (np.dot(displacements[element_dof_mat].reshape(total_nodes, 24), material.element_stiffness) * displacements[element_dof_mat].reshape(total_nodes, 24)).sum(1).reshape(density.shape)
+        self.strain[:] = (np.dot(displacements[element_dof_mat].reshape(total_nodes, 24), material.element_stiffness) * displacements[element_dof_mat].reshape(total_nodes, 24)).sum(1).reshape(density.shape, order="F")
         self.score = (material.stiffness(density).flatten() * self.strain.flatten()).sum()
         self.dc[:] = (-material.gradient(density).flatten() * self.strain.flatten()).reshape(density.shape)
-        self.dv[:] = np.ones(self.shape)
+        self.dv[:] = np.ones(self.shape)  # fixme: dv is constant
+        test = self.dc.copy()
         # Density Filtering (prevents irregular gaps)
         self.dc[:] = Filter.smoothen(self.dc)
         self.dv[:] = Filter.smoothen(self.dv)
@@ -279,13 +276,10 @@ class Optimizer:
             lmid = 0.5 * (l2 + l1)
             # max and min constrict between 0 and 1, move also limits the change
             xnew[:] = np.maximum(0.0, np.maximum(x - move, np.minimum(1.0, np.minimum(x + move, x * np.sqrt(-dc / dv / lmid)))))
-            # gt = g + np.sum((dv * (xnew - x)))
-            # if gt > 0:
-            #     l1 = lmid
-            # else:
-            #     l2 = lmid
             xnew[:] = coerce_elements(xnew)
-            density[:] = Filter.smoothen(xnew)
+            #density[:] = Filter.smoothen(xnew)
+            f = Filter.instance
+            density[:] = np.array(f.H * xnew.flatten("F") / f.Hs.flatten("F")).reshape(density.shape, order="F")
             if density.mean() > volfrac:  #
                 l1 = lmid
             else:
@@ -303,7 +297,9 @@ def get_load(shape: typing.Tuple[int, int, int]):
     # todo: multiple load cases (you may be able to weight importance - allowing for tensile and torsion opt)-> https://www.top3d.app/tutorials/multiple-load-cases-top3d
     y_nodes, x_nodes, z_nodes = shape
     # USER-DEFINED LOAD DOFs - where the forces are (top)
-    il, jl, kl = np.meshgrid(x_nodes, 0, range(0, z_nodes+1))  # Coordinates - this is a vector of forces at the end x distributed along 0 to max z
+    bridge = np.meshgrid(x_nodes, 0, range(0, z_nodes+1))
+    squish = np.meshgrid(range(0, x_nodes+1), y_nodes, range(0, z_nodes+1))
+    il, jl, kl = squish  # Coordinates - this is a vector of forces at the end x distributed along 0 to max z
     loadnid = kl * (x_nodes + 1) * (y_nodes + 1) + il * (y_nodes + 1) + (y_nodes + 1 - jl)  # Node IDs
     load_dof = 3 * loadnid - 2  # DOFs
     num_dofs = 3 * (x_nodes + 1) * (y_nodes + 1) * (z_nodes + 1)  # number of degrees of freedom
@@ -320,7 +316,9 @@ def get_fixed(shape: typing.Tuple[int, int, int]):
     """
     y_nodes, x_nodes, z_nodes = shape
     # USER-DEFINED SUPPORT FIXED DOFs
-    iif, jf, kf = np.meshgrid(0, range(0, y_nodes+1), range(0, z_nodes+1))  # Coordinates - the supports are x=0 along the surface by iterating thorugh y&z
+    bridge = np.meshgrid(0, range(0, y_nodes+1), range(0, z_nodes+1))
+    squish = np.meshgrid(range(0, x_nodes+1), 0, range(0, z_nodes+1))
+    iif, jf, kf = squish  # Coordinates - the supports are x=0 along the surface by iterating thorugh y&z
     fixednid = kf * (x_nodes + 1) * (y_nodes + 1) + iif * (y_nodes + 1) + (y_nodes + 1 - jf)  # Node IDs
     fixed_dof = list(3 * fixednid.flatten() - 1) + list(3 * fixednid.flatten() - 2) + list(3 * fixednid.flatten() - 3)  # DOFs
     return fixed_dof
@@ -338,12 +336,14 @@ def coerce_elements(x: np.ndarray) -> np.ndarray:
 
 
 # === DISPLAY 3D TOPOLOGY (ISO-VIEW) === #
-def display_3d(structure: np.ndarray, strain: np.ndarray, shape):
+def display_3d(structure: np.ndarray, strain: np.ndarray):
     # fixme: figure out the shape
+    structure = np.swapaxes(np.flip(np.swapaxes(structure, 0, 2), 2), 0, 1)  # reorientate the structure
+    strain = np.swapaxes(np.flip(np.swapaxes(strain, 0, 2), 2), 0, 1)
+    shape = structure.shape
     y_nodes, x_nodes, z_nodes = shape
-    strain = np.minimum(1.0, strain.reshape(shape) / strain[structure > 0.1].max())
+    strain = np.minimum(1.0, strain / strain[structure > 0.1].max())
     total_nodes = x_nodes * y_nodes * z_nodes
-    # structure = structure.reshape(shape)  # convert flat array the 3d array matching shape todo: i think this reshape may be wrong
     hue = 2 / 3 - strain * 2 / 3  # get red to blue hue depending on displacement
     saturation = np.ones(shape)  # always high saturation
     value = np.ones(shape)  # always bright
@@ -351,12 +351,11 @@ def display_3d(structure: np.ndarray, strain: np.ndarray, shape):
     rgb = hsv_to_rgb(hsv.reshape((total_nodes, 3))).reshape((*shape, 3))  # convert to accepted format
     alpha = structure.reshape((*shape, 1))
     rgba = np.concatenate((rgb, alpha), axis=3)  # same thing with tranparency equal to density
-    # rgba[:, 0, :] = np.array([0., 0., 0., 1.0])
-    # rgba[0, :, :] = np.array([0., 0., 0., 1.0])
     ax = plt.figure().add_subplot(projection='3d')  # todo: look into interactivity so you can look around
-    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([x_nodes, y_nodes, z_nodes, 1]))
+    ax.set_box_aspect((y_nodes, x_nodes, z_nodes))
     blocks = np.zeros(structure.shape, dtype=bool)
-    blocks[structure > 0.05] = True
+    blocks[structure > 0.5] = True
+    # blocks[0, 0, 4] = True
     ax.voxels(blocks, facecolors=rgba)
     plt.ion()
     plt.show()
@@ -378,7 +377,7 @@ def main(x_nodes: int, y_nodes: int, z_nodes: int, volfrac: float, penal: float,
     # prepare helper classes
     material = Material(0.3, 1.0, 1e-19, penal)  # define the material properties of you structure
     modeler = FEA(shape, material)  # setup FEA analysis
-    smooth_filter = Filter(rmin, shape)  # filter to prevent gaps
+    f = Filter(rmin, shape)  # filter to prevent gaps
 
     # Set loop counter and gradient vectors
     change = 1
@@ -393,14 +392,14 @@ def main(x_nodes: int, y_nodes: int, z_nodes: int, volfrac: float, penal: float,
         # Optimality criteria
         x[:], change = optimizer.optimality_criteria(x, compliance_gradient, volume_gradient, volfrac)
         # Filter design variables
-        density[:] = smooth_filter(x)
+        density[:] = np.array(f.H * x.flatten("F") / f.Hs.flatten("F")).reshape(density.shape, order="F")#smooth_filter(x)
 
         # Write iteration history to screen (req. Python 2.6 or newer)
         print(f"i: {loop} (avg {round((time.time()-start_time)/(loop+1), 2)} sec),\t"
               f"comp.: {round(obj, 3)}\t"
               f"Vol.: {round(density.mean(), 3)*100}%,\t"
               f"ch.: {round(change, 2)}")
-    display_3d(x, optimizer.strain, shape)
+    display_3d(x, optimizer.strain)
     save(x, "test")
 
 
@@ -422,5 +421,5 @@ def run_load():
 
 
 if __name__ == '__main__':
-    run_load()
-    # main(15, 5, 5, 0.2, 4, 1.5)
+    # run_load()
+    main(10, 20, 10, 0.2, 4, 1.5)
